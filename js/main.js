112f593da58458ad2740b2b011990019b2c0e341
Sébastien Ternest <sebt-max@MacBook-Pro-de-Sebastien-Ternest.local>
2022-08-19 00:20:16 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+
+// $(document).ready(function () {
+
+
+//     let $btns = $('.project-area .button-group button');
+
+
+//     $btns.click(function (e) {
+
+//         $('.project-area .button-group button').removeClass('active');
+//         e.target.classList.add('active');
+
+//         let selector = $(e.target).attr('data-filter');
+//         $('.project-area .grid').isotope({
+//             filter: selector
+//         });
+
+//         return false;
+//     })
+
+//     $('.project-area .button-group #btn1').trigger('click');
+
+//     $('.project-area .grid .test-popup-link').magnificPopup({
+//         type: 'image',
+//         gallery: { enabled: true }
+//     });
+
+
+// });
+
+
+const capitalize = (word) => {
+    const firstLetter = word[0].toUpperCase();
+    const restOfTheWord = word.substring(1).toLowerCase();
+    return firstLetter + restOfTheWord;
+};
+
+console.log(capitalize("bEnJaMiN"));
